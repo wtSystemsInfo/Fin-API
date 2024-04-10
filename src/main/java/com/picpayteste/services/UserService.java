01189352a49718f1759eac5070991f15ee12e6ac
wtSystemsInfo <w.t.systems.technology@gmail.com>
2024-04-10 15:26:04 -0300

    feat: create User Service and did some changes at UserRepository

---
+++ src/main/java/com/picpayteste/services/UserService.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.picpayteste.services;
+
+import com.picpayteste.domain.user.User;
+import com.picpayteste.domain.user.UserType;
+import com.picpayteste.repository.UserRepository;
+import java.math.BigDecimal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author William Toloto
+ */
+@Service    
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+    
+    
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception{
+        if(sender.getUserType() == UserType.COMMON){
+            throw new Exception("Usuário LOJISTA não está autorizado a fazer a transação!");
+        }
+        
+        if(sender.getBalance().compareTo(amount) < 0){
+            throw new Exception("Saldo INSUFICIENTE!");
+        }
+    }
+    
+    
+    public User findUserById(Long id) throws Exception{
+        return this.userRepository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado!"));
+        
+    }
+    
+    public void saveUser(User user){
+        this.userRepository.save(user);
+    }
+    
+}
